46725edef3711b1d21fa8b8377a978ce84d2e75b
zainiti <148538402+zainiti@users.noreply.github.com>
2023-10-27 18:47:37 +0900

    Add files via upload

---
+++ 10.27 2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+
+	int n;
+
+	printf("®”‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢B\n");
+	scanf_s("%d", &n);
+
+	if (n % 2 == 1) {
+		printf("Šï”\n");
+	}
+	else {
+		printf("‹ô”\n");
+	}
+
+	return 0;
+}
